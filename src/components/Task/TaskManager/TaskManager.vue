ffedfde03f8ba4d2d03b330379d9d2e1923efa5d
Jorgito <jorgelhd94@gmail.com>
2022-01-28 10:34:19 -0500

    work in toogle tasks

--- src/components/Task/TaskManager/TaskManager.vue
+++ src/components/Task/TaskManager/TaskManager.vue
@@ -23,11 +23,18 @@ function updateText(newText) {
 }
 
 // Task List
-const tasks = ref([]);
+const tasksUndone = ref([]);
+const tasksDone = ref([]);
 
 const getTasks = async () => {
   let q = null;
-  q = query(taskCollection, where('uid', '==', getAuth().currentUser.uid));
+
+  // Tasks undone
+  q = query(
+    taskCollection,
+    where('uid', '==', getAuth().currentUser.uid),
+    where('isDone', '==', false)
+  );
 
   await onSnapshot(
     q,
@@ -40,7 +47,33 @@ const getTasks = async () => {
         };
         listTask.push(task);
       });
-      tasks.value = [...listTask];
+      tasksUndone.value = [...listTask];
+    },
+    (error) => {
+      // ...
+      showMsg('Connection error!! ' + error, 'error');
+    }
+  );
+
+  // Tasks Done
+  q = query(
+    taskCollection,
+    where('uid', '==', getAuth().currentUser.uid),
+    where('isDone', '==', true)
+  );
+
+  await onSnapshot(
+    q,
+    (querySnapshot) => {
+      const listTask = [];
+      querySnapshot.forEach((document) => {
+        const task = {
+          ...document.data(),
+          docID: document.id,
+        };
+        listTask.push(task);
+      });
+      tasksDone.value = [...listTask];
     },
     (error) => {
       // ...
@@ -51,8 +84,15 @@ const getTasks = async () => {
 
 onMounted(getTasks);
 
-const sortTasks = computed(() => {
-  const taskList = [...tasks.value];
+const sortTasksDone = computed(() => {
+  const taskList = [...tasksDone.value];
+  return taskList.sort((a, b) => {
+    return new Date(b.modified_on) - new Date(a.modified_on);
+  });
+});
+
+const sortTasksUndone = computed(() => {
+  const taskList = [...tasksUndone.value];
   return taskList.sort((a, b) => {
     return new Date(b.modified_on) - new Date(a.modified_on);
   });
@@ -69,6 +109,21 @@ const sortTasks = computed(() => {
       @text-editor="updateText"
     />
 
-    <TaskList :tasks="sortTasks" />
+    <TaskList :tasks="sortTasksDone" />
+    <TaskList :tasks="sortTasksUndone" />
+
+    <div
+      v-if="sortTasksDone.length === 0"
+      class="flex flex-col w-full items-center"
+    >
+      <div class="relative">
+        <img width="300" src="../../../assets/img/list.svg" alt="" />
+        <h1
+          class="absolute bottom-0 left-1/4 text-3xl font-bold text-gray-500 tracking-wider"
+        >
+          Empty list
+        </h1>
+      </div>
+    </div>
   </div>
 </template>
